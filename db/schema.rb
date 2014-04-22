beb1505de3e2320454541e4bb2800faa59af13fa
camerondguy@gmail.com <camg@Cams-MacBook-Pro.local>
2014-04-21 20:38:37 -0700

    Revert to e0c69586a23000dd622727885908bdaec1426f29

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140416024259) do
-
+ActiveRecord::Schema.define(version: 20140408011210) do
 
   create_table "users", force: true do |t|
     t.string   "name"
